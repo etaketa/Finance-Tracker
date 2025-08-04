c211c25f416b95bbbdef6793b7889723832b9333
etaketa <erin.ktaketa@gmail.com>
2025-08-04 16:12:36 -0600

    Variables synchronized and goal chart created

--- finance-project/src/pages/Home/Home.jsx
+++ finance-project/src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
-import './Home.css'
+import './Home.css';
+import { Chart as ChartJS, defaults } from 'chart.js/auto';
+import { Doughnut } from 'react-chartjs-2';
+import { useTransactions } from '../Transactions/TransactionContext';
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+defaults.plugins.title.display = true;
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = "black";
 
 function Home() {
 
+    const {total, transactions, goal, setGoal, handleTransaction} = useTransactions();
+    const graph_title = `Goal: $${total.toLocaleString()}/$${goal.toLocaleString()}`;
+
     return(
         <>
-            <h2>Welcome!</h2>
+            <h2>Dashboard</h2>
+            <div className='graph'>
+                <Doughnut
+                    data={{
+                        labels: ["Money", "Money to Save"],
+                        datasets: [{
+                            data: [total, goal - total],
+                            backgroundColor: [
+                                "green",
+                                "tan",
+                            ],
+                        }]
+                    }}
+                    options={{
+                        plugins: {
+                            title: {
+                                text: graph_title,
+                            },
+                        },
+                    }}
+                />
+            </div>
         </>
     );
 }
